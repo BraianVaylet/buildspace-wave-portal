d007e50b53b6309e9c57a92b87d0fcb34c48475f
Braian Vaylet <braianvaylet@gmail.com>
2022-01-12 20:13:42 -0300

    clase 5

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,8 @@
 const main = async () => {
+  // > Para implementar algo en la cadena de bloques, ¡necesitamos tener una dirección de billetera! 
+  // Hardhat hace esto por nosotros en segundo plano, pero aquí tomé la dirección de la billetera
+  // del propietario del contrato y también tomé una dirección de billetera aleatoria y la llamé randomPerson.
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   // > Compilamos nuestro contrato y generamos los archivos necesarios 
   // que necesitamos para trabajar con nuestro contrato en  ./artifacts.
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
@@ -12,6 +16,22 @@ const main = async () => {
   // > waveContract.address nos dará la dirección del contrato desplegado.
   // Esta dirección es cómo podemos encontrar nuestro contrato en la cadena de bloques.
   console.log("Contract deployed to:", waveContract.address);
+  // > owner.address direccion de la billetera que implementa nuestro contrato
+  console.log("Contract deployed by:", owner.address);
+  // > llamo manualmente a nuestras funciones
+  let waveCount;
+  // > llamo a la función para tomar el numero de waves totales.
+  waveCount = await waveContract.getTotalWaves();
+  // > llamo a wave() (con mi direccion).
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  // > vuelvo a obtener el total de waves para ver si cambio.
+  waveCount = await waveContract.getTotalWaves();
+  // > llamo a wave() (con otra direccion, la de randomPerson).
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+  // > vuelvo a obtener el total de waves para ver si cambio.
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
